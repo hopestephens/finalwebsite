9a9ba29b4f65df64114bd1b5556c74954507fcad
hopestephens <46386057+hopestephens@users.noreply.github.com>
2021-05-03 20:45:29 -0400

    mouseover

--- finalwebscript.js
+++ finalwebscript.js
@@ -1,15 +1,9 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
 }
